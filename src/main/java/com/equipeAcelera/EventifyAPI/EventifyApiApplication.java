cdc573b4c88fb11fbccf397079417ff8d87b8cdd
Lucas Ambrosio <lucas.silvaambrosio_9119@outlook.com>
2025-03-07 23:13:18 -0300

    Projeto Base

---
+++ src/main/java/com/equipeAcelera/EventifyAPI/EventifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.equipeAcelera.EventifyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventifyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipeAcelera/EventifyAPI/EventifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipeAcelera.EventifyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
