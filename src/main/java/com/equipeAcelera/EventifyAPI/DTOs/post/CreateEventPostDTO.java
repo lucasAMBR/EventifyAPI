7e188b80b331fa1dccef3f988cfce52891e1aa78
Lucas Ambrosio <lucas.silvaambrosio_9119@outlook.com>
2025-03-22 06:49:54 -0300

    Adiciona função de criação de post de evento

---
+++ src/main/java/com/equipeAcelera/EventifyAPI/DTOs/post/CreateEventPostDTO.java
@@ -0,0 +1,53 @@
+package com.equipeAcelera.EventifyAPI.DTOs.post;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class CreateEventPostDTO {
+    private int userId;
+    private int eventId;
+    private String content;
+    private List<MultipartFile> postImages;
+    
+    public CreateEventPostDTO(int userId, int eventId, String content, List<MultipartFile> postImages) {
+        this.userId = userId;
+        this.eventId = eventId;
+        this.content = content;
+        this.postImages = postImages;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<MultipartFile> getPostImages() {
+        return postImages;
+    }
+
+    public void setPostImages(List<MultipartFile> postImages) {
+        this.postImages = postImages;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    
+}
--- src/main/java/com/equipeAcelera/EventifyAPI/services/PostService.java
+++ src/main/java/com/equipeAcelera/EventifyAPI/services/PostService.java
@@ -7,12 +7,12 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.equipeAcelera.EventifyAPI.DTOs.post.CreateEventPostDTO;
 import com.equipeAcelera.EventifyAPI.DTOs.post.CreatePostDTO;
 import com.equipeAcelera.EventifyAPI.DTOs.post.UpdatePostDTO;
 import com.equipeAcelera.EventifyAPI.exceptions.PersonalExceptions.DataNotFoundException;
+import com.equipeAcelera.EventifyAPI.models.Post.EventPost;
 import com.equipeAcelera.EventifyAPI.models.Post.Post;
-import com.equipeAcelera.EventifyAPI.models.User.NormalUser;
-import com.equipeAcelera.EventifyAPI.models.User.OrganizerUser;
 import com.equipeAcelera.EventifyAPI.models.User.User;
 import com.equipeAcelera.EventifyAPI.utils.AuthUtils;
 import com.equipeAcelera.EventifyAPI.utils.ImageUtils;
@@ -45,13 +45,31 @@ public class PostService {
                 
         postList.add(newPost);
 
-        if(findedUser instanceof NormalUser){
-            ((NormalUser) findedUser).getPostList().add(newPost);
+        findedUser.getPostList().add(newPost);
+
+        return newPost;
+    }
+
+    public Post createEventPost(CreateEventPostDTO postData){
+        if(!AuthUtils.verifyExistentUserById(userService.viewUserList(), postData.getUserId())){
+            throw new RuntimeException("User doesn't exist");
         }
 
-        if(findedUser instanceof OrganizerUser){
-            ((OrganizerUser) findedUser).getPostList().add(newPost);
-        }
+        User findedUser = userService.findUserById(postData.getUserId());
+
+        EventPost newPost = new EventPost(
+            postList.size() + 1, 
+            postData.getUserId(), 
+            findedUser.getName(),
+            postData.getContent(), 
+            ImageUtils.savePostPics(postData.getPostImages()), 
+            new ArrayList<>(),
+            postData.getEventId()
+        );
+                
+        postList.add(newPost);
+
+        findedUser.getPostList().add(newPost);
 
         return newPost;
     }
