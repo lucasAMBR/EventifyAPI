15bd529a82f13a4a5ce6dee7eec5f013a558c41d
Lucas Ambrosio <lucas.silvaambrosio_9119@outlook.com>
2025-04-01 19:38:41 -0300

    Adiciona tratamento das imagens de foto de perfil

--- src/main/java/com/equipeAcelera/EventifyAPI/utils/ImageUtils.java
+++ src/main/java/com/equipeAcelera/EventifyAPI/utils/ImageUtils.java
@@ -1,11 +1,16 @@
 package com.equipeAcelera.EventifyAPI.utils;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.imageio.ImageIO;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,17 +23,38 @@ public class ImageUtils {
             Files.createDirectories(Paths.get(uploadDir));
 
             long timestamp = System.currentTimeMillis();
+            String fileName = timestamp + "_" + profilePic.getOriginalFilename().replaceAll("\\s+", "");
+            String filePath = uploadDir + fileName;
+            File outputFile = new File(filePath);
 
-            // Usei um regex para tirar todos os espacos do nome dos arquivos
-            String fileName = timestamp +"_"+ profilePic.getOriginalFilename().replaceAll("\\s+", "");
-            Path filePath = Paths.get(uploadDir + fileName);
-            Files.write(filePath, profilePic.getBytes());
-            String photoUrl = "/uploads/profile_pic/" + fileName;
-    
-            return photoUrl;
+            BufferedImage originalImage = ImageIO.read(profilePic.getInputStream());
+            BufferedImage croppedImage = cropToSquare(originalImage, 300);
+            ImageIO.write(croppedImage, "jpg", outputFile);
+
+            return "/uploads/profile_pic/" + fileName;
         } catch (IOException e) {
             throw new RuntimeException("Erro ao salvar a imagem", e);
         }
+    }
+
+    private static BufferedImage cropToSquare(BufferedImage source, int size) {
+        int width = source.getWidth();
+        int height = source.getHeight();
+        int minSize = Math.min(width, height);
+
+        BufferedImage cropped = source.getSubimage(
+            (width - minSize) / 2, 
+            (height - minSize) / 2, 
+            minSize, 
+            minSize
+        );
+
+        BufferedImage resized = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = resized.createGraphics();
+        g2d.drawImage(cropped, 0, 0, size, size, null);
+        g2d.dispose();
+
+        return resized;
     }
 
     public static List<String> savePostPics(List<MultipartFile> imageList){
